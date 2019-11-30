207c7e04601f7664d35087b7283e39d042e6bbcd
Victor Gardrinier <v.gardrinier@gmail.com>
2019-11-30 21:55:55 +0100

    add test files

---
+++ test/TestSupplyChain.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+// to complete in the future
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/SupplyChain.sol";
+
+contract TestSupplyChain {
+
+    // Test for failing conditions in this contracts:
+    // https://truffleframework.com/tutorials/testing-for-throws-in-solidity-tests
+
+    // buyItem
+
+    // test for failure if user does not send enough funds
+    // test for purchasing an item that is not for Sale
+
+    // shipItem
+
+    // test for calls that are made by not the seller
+    // test for trying to ship an item that is not marked Sold
+
+    // receiveItem
+
+    // test calling the function from an address that is not the buyer
+    // test calling the function on an item not marked Shipped
+
+}
